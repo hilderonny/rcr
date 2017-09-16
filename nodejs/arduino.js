639b15187a04c1d4b77e81f0b85f6d3993c7c747
hilderonny <gitlab@hildebrandt2014.de>
2017-09-16 21:56:45 +0200

    Head can now be moved by a joystick.

--- nodejs/arduino.js
+++ nodejs/arduino.js
@@ -2,9 +2,27 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-  var servo = new five.Servo({
+  var upDownRange = [50, 170];
+  var leftRightRange = [50, 130];
+  var upDown = new five.Servo({
     pin: 9,
-    range: [50, 170] // Check the endstops!
+    range: upDownRange,
+    center: true
   });
-  servo.sweep();
+  var leftRight = new five.Servo({
+    pin: 8,
+    range: leftRightRange,
+    center: true
+  });
+
+  var joystick = new five.Joystick({
+    pins: ["A0", "A1"],
+    freq: 100
+  });
+
+  joystick.on("change", function() {
+    upDown.to(five.Fn.scale(this.y, -1, 1, upDownRange[0], upDownRange[1]));
+    leftRight.to(five.Fn.scale(this.x, -1, 1, leftRightRange[0], leftRightRange[1]));
+  });
+
 });
